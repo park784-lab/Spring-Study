b4309fe70740b21fdbbf520a77fb817b7d639709
park-note <park-note@NOTE-PARK-ASUS>
2025-09-21 19:59:12 +0900

    454

---
+++ spring/src/main/java/com/spring/spring/first/data/dto/MemberDTO.java
@@ -0,0 +1,15 @@
+package com.spring.spring.first.data.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MemberDTO {
+	String name;
+	String email;
+	String organization;
+
+}
